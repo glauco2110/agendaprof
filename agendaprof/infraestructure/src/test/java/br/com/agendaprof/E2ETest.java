2dbb31188b5e78c3241ec689d2210ce695db3651
Glauco Santos <glaucodejesus2110@gmail.com>
2025-01-30 17:13:55 -0300

    Configurando CI

---
+++ agendaprof/infraestructure/src/test/java/br/com/agendaprof/E2ETest.java
@@ -0,0 +1,16 @@
+package br.com.agendaprof;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-e2e")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public @interface E2ETest {
+}
---
+++ agendaprof/infraestructure/src/test/java/br/com/agendaprof/IntegrationTest.java
@@ -0,0 +1,12 @@
+package br.com.agendaprof;
+
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+public @interface IntegrationTest {
+}
